a47f1bf750e2af062ec88fbf11ded3b81d8eb08a
luca319 <lucasaeoninsurance@gmail.com>
2024-07-31 16:00:26 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ const interviews = [
     title: "Meeting w/ Customer Management section",
     time: "15:00 - 16:00",
     interviewer: "ACSM: Ms. Chandni",
-    interviewee: "Mr. Wan Yunus (SM) no link",
+    interviewee: "Mr. Wan Yunus (SM) (Physical),
     isOnline: true,
     teamsLink:
       "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZjlkYzlkMDctNDJkYi00Y2IxLTllYjctYjRjMGQ1N2I3OWU0%40thread.v2/0?context=%7b%22Tid%22%3a%2221106e7a-134b-4466-9555-3543978c7550%22%2c%22Oid%22%3a%2290f5645c-65fd-4541-b320-ba0814d02249%22%7d",
@@ -121,7 +121,7 @@ const interviews = [
     title: "Meeting w/ Claims Unit",
     time: "10:00 - 11:00",
     interviewer: "ACSM: Ms. Chandni",
-    interviewee: "Mr. Abdul Mu'in (AM) No Link",
+    interviewee: "Mr. Wan Yunus (SM) (Physical)",
     isOnline: true,
     teamsLink:
       "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZDdhNDMxODAtM2M0Ni00NTViLWI1ZDAtMDgzMDM4N2ZjMjY0%40thread.v2/0?context=%7b%22Tid%22%3a%2221106e7a-134b-4466-9555-3543978c7550%22%2c%22Oid%22%3a%22b253f2f1-a78d-4903-8a92-3f98323ebc5d%22%7d",
